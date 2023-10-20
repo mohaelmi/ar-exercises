3168edefe7be46089f77ded5a92794a813d51615
mohaelmi <moha.abdi1771@gmail.com>
2023-10-20 16:11:31 -0500

    exercise_6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+
+class Store < ActiveRecord::Base
+  belongs_to :store
+end
+
+# Add some data into employees. Here's an example of one (note how it differs from how you create stores):
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Moha", last_name: "Elmi", hourly_rate: 190)
+@store2.employees.create(first_name: "Jim", last_name: "Bob", hourly_rate: 50)
+@store2.employees.create(first_name: "Vicky", last_name: "Amenual", hourly_rate: 80)
+
